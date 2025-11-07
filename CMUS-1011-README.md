2af8dae952370db15cc65bd0d8eaac1ea6c3e103
Clément <clement.rames@gmail.com>
2025-11-07 17:16:27 +0100

    added examples of different network types

--- CMUS-1011-README.md
+++ CMUS-1011-README.md
@@ -1,7 +1,7 @@
 # 1. Introduction  
 For today’s session, we will use R to explore some basic network science techniques.  
 Make sure you have the [igraph](https://r.igraph.org), [ggraph](https://ggraph.data-imaginist.com) and [tidygraph](https://tidygraph.data-imaginist.com) libraries installed.  
-To get started with network science in R, see this great [intro tutorial](https://www.r-bloggers.com/2021/06/using-geospatial-data-in-r/)  
+To get started with network science in R, see this great [intro tutorial](https://r.igraph.org/articles/igraph.html)  
 If you wish to dive deeper into network science in R, here are two good (free) references:  
 - [CRAN](https://cran.r-project.org/web/views/NetworkAnalysis.html)  
 - [LADAL](https://slcladal.github.io/net.html)  
@@ -9,3 +9,14 @@ If you wish to dive deeper into network science in R, here are two good (free) r
 # 2. Data
 For this practice session, you are free to download network data of your choice from [SNAP](https://snap.stanford.edu/data/).  
 For example, i chose to download a [Facebook social network](https://snap.stanford.edu/data/ego-Facebook.html) dataset.  
+
+# 3. Methods
+The attached script first walks you through basic network types and essential methods.  
+
+# 4. Exploring your real world network  
+Go ahead and apply the same methods on your real-world network as with the above toy networks:  
+- Degree distribution
+- Edge betweenness 
+- Community detection 
+- Plotting/visualization 
+BONUS QUESTION: what kind of network does your real world network look like?
--- CMUS-1011-completed.R
+++ CMUS-1011-completed.R
@@ -19,11 +19,72 @@ links <- data.frame(
 network <- graph_from_data_frame(d = links, directed = F)
 plot(network)
 
-# Now, let's start playing with the real world network that you downloaded from
+# Third, we'll start exploring different kinds of networks
+
+# Let's create an Erdős-Rényi random network with 11 nodes and 22 edges
+g <- sample_gnm(11, 22)
+plot(g)
+
+# Let's see what's the degree distribution in this network
+degree(g)
+degree_distribution(g)
+
+# What about the edge betweenness?
+edge_betweenness(g)
+ebs <- edge_betweenness(g)
+as_edgelist(g)[ebs == max(ebs), ]
+
+# Let's detect communities in this network
+comm <- cluster_louvain(g)
+plot(comm, g)
+
+# Let's plot this same graph using a force-directed layout
+layout <- layout_with_fr(g)
+plot(g, layout = layout)
+
+# Let's create a Watts-Strogatz small world network with 150 nodes
+g <- sample_smallworld(1, 150, 5, 0.05)
+plot(g)
+#Let's compute the mean distance between any two nodes in the network
+mean_distance(g)
+
+# Create a Barabasi-Albert scale-free network (for example, the World Wide Web)
+g <- sample_pa(200, power = 1, m = 1, directed = FALSE)
+# Customize the graph appearance and visualize node degree and edge betweenness
+V(g)$size <- degree(g) # Vertex size based on degree
+V(g)$color <- rainbow(100)[cut(degree(g), breaks = 100)] # Vertex color based on degree
+E(g)$width <- edge_betweenness(g) * 0.001 # Edge width based on betweenness
+plot(g,
+     vertex.label = NA, # Remove vertex labels
+     edge.curved = 0.1, # Add curve to edges
+     layout = layout_with_fr(g))
+
+# Create and visualize a weighted graph (for example, the strength of relationships)
+edges <- data.frame(
+  from = sample(1:11, 22, replace = TRUE),
+  to = sample(1:11, 22, replace = TRUE),
+  weight = runif(22))
+g <- graph_from_data_frame(edges, directed = FALSE)
+plot(g,
+     edge.width = E(g)$weight * 10, # Edge width based on weight
+     vertex.size = 20,
+     vertex.color = "lightblue",
+     vertex.label.color = "black",
+     edge.color = "gray50",
+     layout = layout_in_circle(g))
+
+
+# Finally, let's start playing with the real world network that you downloaded from
 # https://snap.stanford.edu/data/
 fb <- read.table("/Users/Downloads/facebook_combined.txt") # a facebook dataset
 gfb <- graph_from_data_frame(fb, directed = FALSE)
-plot(gfb)
 em <- read.table("/Users/Downloads/email-Eu-core.txt") # an email dataset
 gem <- graph_from_data_frame(em, directed = TRUE)
-plot(gem)
+
+# Go ahead and apply the same methods on your network as with the above toy networks:
+# Degree distribution
+# Edge betweenness
+# Community detection
+# Plotting/visualization
+
+# BONUS QUESTION: what kind of network does your real world network look like?
